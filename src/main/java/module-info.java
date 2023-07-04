7e4c60b284f19edf1af2af73ac50b9a27282be24
jastpl2153 <jastpl2153@mail.ru>
2023-07-04 22:03:33 +0800

    added an incomplete function for calculating the number system

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,9 +4,10 @@ module com.example.calculator {
 
     opens com.example.calculator to javafx.fxml;
     exports com.example.calculator;
-    opens com.example.calculator.controller to javafx.fxml;
 
+    opens com.example.calculator.controller to javafx.fxml;
     exports com.example.calculator.controller;
+
     exports com.example.calculator.controller.converter;
     opens com.example.calculator.controller.converter to javafx.fxml;
 }
